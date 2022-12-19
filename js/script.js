b6ea5ce3ea80bcd08453acc296b41c84bc93aef0
joao victor <thehoodgamer20@gmail.com>
2022-12-18 22:46:39 -0300

    adicionado o array de objeto data no app.js

--- js/script.js
+++ js/script.js
@@ -18,15 +18,6 @@ console.log(formatarDinheiro(8000));
 console.log(fileiraEspecial(cliente));
 console.log(maiorValor(collection));
 
-
-const data = [
-  { name: "Matheus", age: 31, sallary: 2000, driverLicense: true },
-  { name: "João", age: 18, sallary: 1500, driverLicense: false },
-  { name: "Mariana", age: 22, sallary: 4000, driverLicense: true },
-  { name: "Pedro", age: 50, sallary: 7200, driverLicense: true },
-  { name: "Érica", age: 16, sallary: 0, driverLicense: false },
-];
-
 const array = ["João Victor", "Clemilton", "Gabriel", "Léo"];
 
 //Método Reverse
